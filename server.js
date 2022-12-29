a38046cd05173499a744a31a67c8979edceb387d
hacker31287 <87894845+hacker31287@users.noreply.github.com>
2022-12-30 03:24:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 // interprets commands and delegates the actual command
 // running to a Command instance. When started, an owner
 // ID should be given.
-
+const express = require('express');
+const app = express();
 var path = require("path");
 var fs = require("fs");
 var botgram = require("botgram");
@@ -509,4 +510,13 @@ bot.command("help", function (msg, reply, next) {
 
 bot.command(function (msg, reply, next) {
   reply.reply(msg).text("Invalid command.");
+
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
 });
